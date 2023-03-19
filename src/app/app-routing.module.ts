36473a4c01b190f0e9fac87f4429101803632328
ushansankalpa <ushan.20191142@iit.ac.lk>
2023-03-20 00:06:56 +0530

    added files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { LSIdentiferTestComponent } from './components/lsidentifer-test/lsidentifer-test.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 const routes: Routes = [
   {
     path:'lsidentifertest',
     component: LSIdentiferTestComponent
   },
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path:'navbar',
+    component: NavBarComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,28 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LSIdentiferTestComponent } from './components/lsidentifer-test/lsidentifer-test.component';
-import {CardModule} from 'primeng/card';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LSIdentiferTestComponent
+    LSIdentiferTestComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    AdminDashboardComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule,
-    CardModule
+    BrowserModule,
+    AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
